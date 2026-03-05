7cf4cb8026afe404b6465bbbe3dc675191e1179a
Nama Anda <dhanzymediatech@gmail.com>
2026-03-05 14:49:03 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,13 @@
+/*
+===================================================
+FILE : script.js
+DESKRIPSI :
+JavaScript untuk membuat website interaktif
+===================================================
+*/
+
+function sayHello() {
+
+    alert("Halo! Terima kasih sudah mengunjungi portfolio saya 😊")
+
+}
